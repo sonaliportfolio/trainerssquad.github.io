3ab847ea7f873074675312f260d75bf6eb5d4dd9
s-sanap <sonalisanap221999@gmail.com>
2025-12-16 14:03:39 +0530

    changes

--- about-us.php
+++ about-us.php
@@ -6,6 +6,7 @@
 	<meta charset="utf-8" />
 	<title>About-Trainner </title>
 	<meta name="description" content="" />
+	<!-- responsive tag -->
 	<meta http-equiv="x-ua-compatible" content="ie=edge" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<!-- favicon -->
